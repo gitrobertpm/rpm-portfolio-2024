60784a817ce5b08df6b7a56ebc5a3797c4e594a9
robertpm <grobertpm@gmail.com>
2024-02-05 00:56:42 -0800

    Started setting up configs for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/rpm-portfolio-2024/",
   plugins: [
     vue(),
     vueJsx(),
@@ -17,9 +18,9 @@ export default defineConfig({
   },
   css: {
     preprocessorOptions: {
-        scss: {
-            additionalData: `@import "@/assets/scss/global.scss";`,
-        },
+      scss: {
+        additionalData: `@import "@/assets/scss/global.scss";`,
+      },
     },
-},
+  },
 })
